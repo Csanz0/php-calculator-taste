7ee068d46db9d9110d71921a1c1c008bda1371c4
Csc32 <sanzonettycar@gmail.com>
2022-06-19 15:45:01 -0400

    fix responsive design

--- index.php
+++ index.php
@@ -82,19 +82,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Operaciones numericas en PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
    <header class="container-fluid bg-primary p-2">
        <h1 class="h1 text-light">Calculadora Simple</h1>
    </header> 
    <div class="container-fluid bg-light d-flex flex-row flex-wrap justify-content-center aling-item-center">
-        <div class="container-md d-sm-flex flex-row flex-wrap justify-content-center align-item-center">
+        <div class="container-lg d-lg-flex flex-row flex-wrap justify-content-center align-item-center">
             <form method="post">
-            <div class="container-lg m-3 d-flex flex-md-column flex-md-row">
+            <div class="container-lg-fluid m-3 d-lg-flex flex-lg-column flex-sm-wrap">
              <label class="form-label h2 mb-2">Ingrese 3 numeros</label>   
-            <div class="container-fluid mb-2 d-flex justify-content-center">
-                    <input type="text" class="form-control-lg flex-fill p-5 text-end" readonly value="<?php
+            <div class="container-fluid mb-2 d-flex justify-content-md-center">
+                    <input type="text" class="form-control-lg p-5 w-100 flex-lg-{grow|shrink}-1" readonly value="<?php
                     if (isset($_POST['btn'])) {
                         verifyOperation($_POST['number1'],$_POST['number2'],$_POST['number3'],$_POST['operation']);
                     }else {
@@ -102,7 +102,7 @@
                     }
                     ?>" >
             </div>
-                <div class="container-fluid mb-2 d-lg-flex flex-lg-row flex-xxl-wrap ">
+                <div class="container-fluid mb-2 d-lg-flex flex-lg-row flex-xxl-wrap flex-sm-{grow-shrink}-1">
 
                     <input type="text" name="number1" placeholder="0" class="form-control m-1" value="<?php isset($_POST['number1']) && is_numeric($_POST['number1']) ?print $_POST['number1']:"";?>">
                     
@@ -111,7 +111,7 @@
                     <input type="text" name="number3"  placeholder="0" class="form-control m-1" value="<?php isset($_POST['number3']) && is_numeric($_POST['number3']) ?print $_POST['number3']:"";?>">
                     
                 </div>
-                <div class="container-fluid mb-2">
+                <div class="container-fluid mb-2 flex-sm-{grow-shrink}-1">
                 <select name="operation" id="" class="form-select bg-light">
                 <option value="">Seleccione una Operación</option>
 
@@ -125,7 +125,7 @@
 
                 </select>
                 </div>
-                <div class="container-fluid d-flex flex-column flex-wrap justify-content-center mb-3">
+                <div class="container-fluid d-flex flex-column flex-wrap justify-content-center mb-3 flex-sm-{grow-shrink}-1">
                     <input type="submit" name="btn" id="" value="enviar" class="btn btn-primary flex-{grow|shrink}-1"></div>
             </div>
 
@@ -133,7 +133,7 @@
         </div>
 
    </div>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
 </html>
